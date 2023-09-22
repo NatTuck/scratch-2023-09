dcb33ee6315bd7b32f7cfe101fd012030081131d
Nat Tuck <nat@ferrus.net>
2023-09-22 12:05:43 -0400

    ds lecture 11

--- DS/11/list-vs-array/src/main/java/demo/ArrayWrap.java
+++ DS/11/list-vs-array/src/main/java/demo/ArrayWrap.java
@@ -13,27 +13,59 @@ import java.util.function.Function;
 public class ArrayWrap<T> {
     T[] data;
 
-    ArrayWrap<T>() {
-        ArrayWrap(0);
+    ArrayWrap() {
+        this.data = (T[]) new Object[0];
     }
 
-    ArrayWrap<T>(int nn) {
-        this.data = new T[nn];
+    ArrayWrap(int nn) {
+        this.data = (T[]) new Object[nn];
+    }
+
+    int length() {
+        return data.length;
     }
 
     T get(int ii) {
         return data[ii];
     }
 
-    void put(int ii, T vv) {
+    void set(int ii, T vv) {
         data[ii] = vv;
     }
 
     void resize(int nn) {
-        T[] data1 = new T[nn];
-        for (int ii = 0; ii < Math.min(nn, this.data.length); ++ii) {
-            data1[ii] = this.data[ii];
+        T[] data1 = (T[]) new Object[nn];
+        for (int ii = 0;
+             ii < Math.min(this.data.length, data1.length);
+             ++ii) {
+            data1[ii] = data[ii];
         }
         this.data = data1;
+    }
+
+    ArrayWrap<T> map(Function<T, T> op) {
+        var ys = new ArrayWrap<T>(this.data.length);
+        for (int ii = 0; ii < this.data.length; ++ii) {
+            ys.set(ii, op.apply(this.get(ii)));
+        }
+        return ys;
+    }
+
+    void forEach(Function<T, T> op) {
+        for (int ii = 0; ii < this.data.length; ++ii) {
+            this.set(ii, op.apply(this.get(ii)));
+        }
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        sb.append("[");
+        for (int ii = 0; ii < this.data.length; ++ii) {
+            sb.append(this.get(ii).toString());
+            sb.append(" ");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
