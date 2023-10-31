993ebeb2636e852f25bbe97a074ce1378e3a9567
Nat Tuck <nat@ferrus.net>
2023-10-31 19:12:38 -0400

    26

---
+++ DS/26/demo/src/main/java/demo/App.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        var sounds = new HashMap<String, String>();
+        sounds.put("cow", "moo");
+        sounds.put("dog", "arf");
+        System.out.println("cow says " + sounds.get("cow"));
+    }
+}
